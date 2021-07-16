d837c2ea4f5e39f50983e59ec6edec1e5e56116e
Erica Noronha <erica.noronha@live.com>
2021-07-16 02:18:08 -0300

    aula 5.3
    service e injeção de dependência

---
+++ src/app/services/transferencia.service.ts
@@ -0,0 +1,31 @@
+
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransferenciaService {
+
+  private listaTransferencia: any[];
+
+  constructor() {
+    this.listaTransferencia = [];
+  }
+
+  get transferencias() {
+    return this.listaTransferencia;
+  }
+
+  adicionar(transferencia: any){
+    this.hidratar(transferencia);
+    this.listaTransferencia.push(transferencia);
+    // console.log($event);
+    // const transferencia = {...$event, data: new Date()};
+    // this.transferencias.push(transferencia);
+  }
+
+  private hidratar(transferencia: any){
+    transferencia.data = new Date();
+
+  }
+}
