a72ed7a604bef727e01d7a26b6cd52e0c1879427
Erica Noronha <erica.noronha@live.com>
2021-07-16 01:12:40 -0300

    conteúdo até aula 4.3

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'bytebank';
+
+  // valor: number;
+  // destino: number;
+  transferencia: any;
+
+  transferir($event){
+    console.log($event);
+    // this.destino = $event.destino;
+    // this.valor = $event.valor;
+    this.transferencia = $event;
+  }
+
+}
